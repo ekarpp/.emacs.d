5d08cccd30690a075363464a0ea4db26e42c9ba6
eetuka <eetu.karppinen@aalto.fi>
2019-03-20 14:25:37 +0200

    elget+luamode

--- el-get/.status.el
+++ el-get/.status.el
@@ -1,4 +1,6 @@
-((el-get status "installed" recipe
+((ample-regexps status "installed" recipe
+                (:name ample-regexps :description "Compose and reuse Emacs regular expressions with ease" :type github :pkgname "immerrr/ample-regexps.el"))
+ (el-get status "installed" recipe
          (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
                 ("el-get.*\\.el$" "methods/")
                 :features el-get :post-init
@@ -19,4 +21,8 @@
                       (dolist
                           (feat feats)
                         (unload-feature feat t))))
-                  (require 'el-get)))))
+                  (require 'el-get))))
+ (lua-mode status "installed" recipe
+           (:name lua-mode :description "A major-mode for editing Lua scripts" :depends
+                  (ample-regexps)
+                  :type github :pkgname "immerrr/lua-mode")))
